e1b36bb096507dfaf84a09f71fb8d43df394d0d7
PreetAsari15 <preetasari@gmail.com>
2024-04-08 12:21:54 -0600

    new libraries - Spring. Added Assets with glbs and images. Code update

--- 3D_Visuals/src/pages/Home.jsx
+++ 3D_Visuals/src/pages/Home.jsx
@@ -2,8 +2,21 @@ import React from "react";
 import { Canvas } from "@react-three/fiber";
 import { Suspense } from "react";
 import Loader from "../components/Loader";
-
+import Space from "../models/SpaceBoi";
 const Home = () => {
+  const adjustSpaceForScreenSize = () => {
+    let screenScale = null;
+   let screenPosition = [0, -6.5,-43];
+   let rotation = [0.1, 4.7, 0];
+
+    if(window.innerWidth < 768) {
+      screenScale = [0.9, 0.9, 0.9];
+    }else{
+      screenScale = [3, 3, 3];
+    }
+    return [screenScale, screenPosition, rotation];
+  }
+  const [spaceScale, spacePositioin, spaceRoation] = adjustSpaceForScreenSize();
   return (
     // <div className='absolute top-28 left-0 right-0 z-0 flex items-center justify-center'>
     //   POPUP
@@ -12,15 +25,20 @@ const Home = () => {
       <Canvas
         className="w-full h-screen bg-transparent"
         camera={{ near: 0.1, far: 1000 }}
-      ></Canvas>
+      >
       <Suspense fallback={<Loader />}>
           <directionalLight />
           <ambientLight />
           <pointLight />
           <spotLight />
           <hemisphereLight />
-
+        <Space
+        position = {spacePositioin}
+        scale = {spaceScale}
+        rotation = {spaceRoation}
+        />
       </Suspense>
+      </Canvas>
     </section>
   );
 };
