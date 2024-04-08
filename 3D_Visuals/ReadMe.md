60685add0c0012268ba5735f20b2a0de83bcc315
PreetAsari15 <preetasari@gmail.com>
2024-04-08 10:53:19 -0600

    more changes

--- 3D_Visuals/ReadMe.md
+++ 3D_Visuals/ReadMe.md
@@ -6,3 +6,11 @@ Currently, two official plugins are available:
 
 - [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
 - [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+
+
+# React Three Fiber
+Three js library
+
+Now, to render something in Canvas, it has to be special 3D property.
+
+We use React drei
--- 3D_Visuals/src/pages/Home.jsx
+++ 3D_Visuals/src/pages/Home.jsx
@@ -1,14 +1,28 @@
-import React from 'react'
+import React from "react";
+import { Canvas } from "@react-three/fiber";
+import { Suspense } from "react";
+import Loader from "../components/Loader";
 
 const Home = () => {
   return (
-      // <div className='absolute top-28 left-0 right-0 z-0 flex items-center justify-center'>
-      //   POPUP
-      // </div>
-    <section className='w-full h-screen relative'>
+    // <div className='absolute top-28 left-0 right-0 z-0 flex items-center justify-center'>
+    //   POPUP
+    // </div>
+    <section className="w-full h-screen relative">
+      <Canvas
+        className="w-full h-screen bg-transparent"
+        camera={{ near: 0.1, far: 1000 }}
+      ></Canvas>
+      <Suspense fallback={<Loader />}>
+          <directionalLight />
+          <ambientLight />
+          <pointLight />
+          <spotLight />
+          <hemisphereLight />
 
+      </Suspense>
     </section>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
