ea7dcb3919b4272533c141e6993a10598cf4b7f7
PreetAsari15 <preetasari@gmail.com>
2024-04-08 15:15:09 -0600

    rotation update

--- 3D_Visuals/src/pages/Home.jsx
+++ 3D_Visuals/src/pages/Home.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { Canvas } from "@react-three/fiber";
-import { Suspense } from "react";
+import { useState, Suspense } from "react";
 import Loader from "../components/Loader";
 import Space from "../models/SpaceBoi";
 const Home = () => {
+  const [isRotating, setIsRotating] = useState(false)
   const adjustSpaceForScreenSize = () => {
-    let screenScale = null;
+  let screenScale = null;
    let screenPosition = [0, -6.5,-43];
    let rotation = [0.1, 4.7, 0];
 
@@ -23,19 +24,19 @@ const Home = () => {
     // </div>
     <section className="w-full h-screen relative">
       <Canvas
-        className="w-full h-screen bg-transparent"
+        className={`w-full h-screen bg-transparent ${isRotating ? 'cursor-grabbing' : 'cursor-grab'}`}
         camera={{ near: 0.1, far: 1000 }}
       >
       <Suspense fallback={<Loader />}>
-          <directionalLight />
-          <ambientLight />
-          <pointLight />
-          <spotLight />
-          <hemisphereLight />
+          <directionalLight position={[1, 1, 1]} intensity={1}/>
+          <ambientLight intensity={0.5}/>
+          <hemisphereLight skyColor="#b1e1ff" groundColor="#000000" intensity={1}/>
         <Space
         position = {spacePositioin}
         scale = {spaceScale}
         rotation = {spaceRoation}
+        isRotating = {isRotating}
+        setIsRotating = {setIsRotating}
         />
       </Suspense>
       </Canvas>
